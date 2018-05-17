503b24c863f1a128e03cd68b5aa1536ed47a893c
jithindasad <jithindasad@gmail.com>
2018-05-17 18:41:30 +0530

    Add mongoose and store data instead of array

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express    = require("express"),
+    app        = express(),
+    bodyParser = require("body-parser"),
+    mongoose   = require("mongoose")
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-  {name: "kerala", image: "https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b0144393f1c870a3e4bc_340.jpg"},
-  {name: "goa", image: "https://pixabay.com/get/ea36b7062bf6093ed1584d05fb1d4e97e07ee3d21cac104497f6c070aee9bcb1_340.jpg"},
-  {name: "delhi", image: "https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg"},
-  {name: "kodai", image: "https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg"},
-  {name: "kodak", image: "https://farm8.staticflickr.com/7268/7121859753_e7f787dc42.jpg"},
-  {name: "wayanad", image: "https://farm8.staticflickr.com/7258/7121861565_3f4957acb1.jpg"},
-];
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get("/", function(req, res){
   res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-  res.render("campgrounds", {campgrounds: campgrounds});
+  Campground.find({}, function (err, campgrounds) {
+    if (err) {
+      console.log(err);
+    }else {
+      res.render("campgrounds", {campgrounds: campgrounds});
+    }
+  })
 });
+
 
 app.post("/campgrounds", function(req, res){
   //get element from form
@@ -28,8 +35,13 @@ app.post("/campgrounds", function(req, res){
   var image = req.body.image;
   //push to array
   var newCampground = {name: name, image: image};
-  campgrounds.push(newCampground);
-  res.redirect("campgrounds");
+  Campground.create(newCampground, function(err, newlyCreated){
+    if (err) {
+      console.log(err);
+    }else {
+      res.redirect("/campgrounds");
+    }
+  })
 });
 
 app.get("/campgrounds/new", function(req, res){
